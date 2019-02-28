a96d3fdf8421ee9f5daa969c1ad4e6673732b0f5
Roesler <roeslerj1@student.douglascollege.ca>
2019-02-28 14:58:50 -0800

    Added comment test

--- Assignment1_Group01.php
+++ Assignment1_Group01.php
@@ -4,12 +4,24 @@ require_once('inc\Page.class.php');
 
 HTML_Header();
 
+/**
+ * Assignment 01
+ * 2019-02-28
+ * Jonah
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     FileAgent::writeToFile($_POST);
 }
-
-
-
 
 HTML_Footer();
 ?>
