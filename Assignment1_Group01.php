386de5af77df1c4d010ccd67602abf4f0aae907e
Jonah <Jonah.Roesler@Gmail.com>
2019-03-07 00:35:41 -0800

    created more of person class and added structure for moving up and down person array

--- Assignment1_Group01.php
+++ Assignment1_Group01.php
@@ -15,10 +15,54 @@ HTML_Header();
  * 
  */
 
+// <INPUT type = "submit" name = "btnPrevious" value = "Previous"/>
+// <INPUT type = "submit" name = "btnSave" value = "Save"/>
+// <INPUT type = "submit" name = "btnDelete" value = "delete"/>
+// <INPUT type = "submit" name = "btnNext" value = "Next"/>
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+//UNIMPLEMENTED METHODs
+//INITIALIZE PAGE
+$newPerson = new Person();
+$peopleArray = array();
+$peopleArray[] = $newPerson;
+$pIndex = 0;
+foreach(FileAgent::ReadPeople() as $rPerson) {
+    $peopleArray[] = $rPerson;
+}
+
+Page::HTML_Header();
+//Person 0 is the new person or blank person.
+Page::HTML_WriteForm($peopleArray[$pIndex]);
+
+if(isset($_POST['btnSave'])) {
     FileAgent::writeToFile($_POST);
 }
+if(isset($_POST['btnNext'])) {
+
+    if((++$pIndex) >= count($peopleArray)) {
+    }
+    else {
+        flush();
+        $pIndex++;
+        Page::HTML_Header();
+        Page::HTML_WriteForm($peopleArray[$pIndex]);
+    }
+}
+if(isset($_POST['btnPrevious'])) {
+    if((--$pIndex) < 0) {
+    }
+    else {
+        flush();
+        $pIndex--;
+        Page::HTML_Header();
+        Page::HTML_WriteForm($peopleArray[$pIndex]);
+    }
+
+}
+if(isset($_POST['btnDelete'])) {
+    FileAgent::writeToFile($_POST);
+}
+
 
 HTML_Footer();
 ?>
